ec9e4cecf2b8a32ff3a48e1986574a476c194465
Stephanie <stphngrr@bitbucket.org>
2020-04-23 18:37:07 -0700

    Combined both submissions into one branch

---
+++ TeamCode/src/main/java/com/acmerobotics/robot/OpModes/Auto2.java
@@ -0,0 +1,172 @@
+package com.acmerobotics.robot.OpModes;
+
+import com.acmerobotics.robot.Robot.Drive;
+import com.acmerobotics.robot.Robot.Launcher;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class Auto2 extends LinearOpMode {
+
+    private int state;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Drive drive = new Drive(hardwareMap, false);
+        Launcher launcher = new Launcher(hardwareMap);
+
+        state = 0;
+
+        drive.resetEncoders();
+
+        waitForStart();
+
+        while(!isStopRequested()){
+
+            switch (state) {
+
+                case 0:
+
+                    drive.goToPosition(24, 0.5);
+                    state++;
+                    break;
+
+                case 1:
+
+                    if(drive.atLinearPos()){
+                        drive.stopMotors();
+
+                    } else {
+
+                        drive.resetAngle();
+                        state++;
+                    }
+
+                    break;
+
+                case 2:
+
+
+                    drive.setDegrees(-30);
+
+                    drive.getDegrees();
+
+                    if(drive.getAngle() == 0) {
+                        drive.clockwise();
+                    }
+
+                    if(drive.getDegrees() > 0) {
+
+                        if(drive.getAngle() < drive.getDegrees()){
+                            drive.counterClockwise();
+
+                        } else {
+
+                            drive.stopMotors();
+                            state++;
+                        }
+
+                    } else {
+
+                        if(drive.getAngle() > drive.getDegrees()){
+                            drive.clockwise();
+
+                        } else {
+
+                            drive.stopMotors();
+                            state++;
+                        }
+
+                    }
+
+                    break;
+
+                case 3:
+
+                    launcher.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                    launcher.setPower(1);
+                    state++;
+
+                    break;
+
+
+                case 4:
+
+                    if (launcher.isRunning()){
+
+                        launcher.openGate();
+                        drive.resetAngle();
+                        state++;
+
+                    }
+
+                    break;
+
+
+                case 5:
+
+                    drive.setDegrees(-60);
+
+                    drive.getDegrees();
+
+                    if(drive.getAngle() == 0) {
+                        drive.clockwise();
+                    }
+
+                    if(drive.getDegrees() > 0) {
+
+                        if(drive.getAngle() < drive.getDegrees()){
+                            drive.counterClockwise();
+
+                        } else {
+
+                            drive.stopMotors();
+                            state++;
+                        }
+
+                    } else {
+
+                        if(drive.getAngle() > drive.getDegrees()){
+                            drive.clockwise();
+
+                        } else {
+
+                            drive.stopMotors();
+                            state++;
+                        }
+
+                    }
+
+                    break;
+
+                case 6:
+
+                    drive.resetEncoders();
+                    state++;
+
+                    break;
+
+
+                case 7:
+
+                    drive.goToPosition(36, 0.5);
+                    state++;
+
+                    break;
+
+                case 8:
+
+                    if(drive.atLinearPos()){
+                        drive.stopMotors();
+
+                    }
+
+                    break;
+
+
+            }
+
+
+        }
+
+    }
+}
